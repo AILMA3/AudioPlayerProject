6560f2d4fecbbe144ce84022b458dd66efb8c544
Savelii Chikshov <tygdym06@mail.ru>
2025-11-09 15:00:54 +0300

    Добавьте файлы проекта.

---
+++ Models/AudioTrack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class AudioTrack
+{
+    public string FilePath { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+
---
+++ Models/Playlist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Playlist
+{
+    public string Name { get; set; }
+    public ObservableCollection<AudioTrack> Tracks { get; set; }
+}
